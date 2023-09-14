7994e65bfa7dd333c188e9e662c20d8b15a6d41b
VIIBII <birlavishal90@gmail.com>
2023-09-14 11:27:52 +0530

    submitting code-Thu Sep 14 2023 11:27:51 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,127 +1,24 @@
-import React, { useState, useEffect } from "react";
-
-import Loading from "./Loading";
-import Tours from "./Tours";
-
-const url = "https://course-api.com/react-tours-project";
+import React from "react";
+import Navbar from "./Navbar";
+// import CartContainer from "./CartContainer";
+import { useGlobalContext } from "./context";
 
 const App = () => {
-  const [tours, setTours] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  const removeTour = (id) => {
-    setTours(tours.filter((tour) => tour.id !== id));
-  };
-
-  useEffect(() => {
-    fetchTours();
-  }, []);
-
-  const fetchTours = async () => {
-    try {
-      setLoading(true);
-      const response = await fetch(url);
-
-      const toursFetched = await response.json();
-      setTours(toursFetched);
-      setLoading(false);
-    } catch (err) {
-      setLoading(false);
-      console.log(err);
-    }
-  };
+  const { loading } = useGlobalContext();
 
   if (loading) {
     return (
-      <main>
-        <Loading />
-      </main>
-    );
-  }
-
-  if (tours.length === 0) {
-    return (
-      <main id="main">
-        <div className="title">
-          <h2>No tours left</h2>
-          <button className="btn" onClick={fetchTours}>
-            refresh
-          </button>
-        </div>
-      </main>
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
     );
   }
   return (
     <main id="main">
-      <Tours tours={tours} removeTour={removeTour} />
+      <Navbar />
+      {/* <CartContainer /> */}
     </main>
   );
 };
-
-// class App extends React.Component{
-
-//   constructor(props){
-//     super(props)
-//     this.state={
-//       tours:[],
-//       loading: true
-//     }
-
-//     this.fetchTours = this.fetchTours.bind(this)
-//     this.removeTour = this.removeTour.bind(this)
-//   }
-
-//   removeTour(id){
-//     this.setState({tours:this.state.tours.filter((tour)=>tour.id!==id)})
-//   }
-//   // fetchTours = async () => {
-
-//   // }
-
-//   componentDidMount(){
-//     this.fetchTours()
-//   }
-//   async fetchTours (){
-//     try{
-//       this.setState({loading:true})
-//       const response = await fetch(url)
-
-//       const toursFetched = await response.json()
-//       console.log(toursFetched)
-//       this.setState({tours:toursFetched,loading:false})
-//     }catch(err){
-//       this.setState({loading:false})
-//       console.log(err)
-
-//     }
-//   }
-
-//   render(){
-
-//     if(this.state.loading){
-//       return(
-//         <main>
-//           <Loading />
-//         </main>
-//       )
-//     }
-
-//     if(this.state.tours.length === 0 ){
-//       return(
-//           <main>
-//             <div className="title">
-//               <h2>No tours left</h2>
-//               <button className="btn" onClick={this.fetchTours}>refresh</button>
-//             </div>
-//           </main>
-//       )
-//     }
-//     return(
-//       <main>
-//         <Tours tours={this.state.tours} removeTour={this.removeTour}/>
-//       </main>
-//     )
-//   }
-// }
 
 export default App;
